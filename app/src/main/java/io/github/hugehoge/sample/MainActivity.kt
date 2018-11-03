ec6f691054620af4fcca43bae2866f23d13052f3
hugehoge <hugehoge42@gmail.com>
2018-11-04 04:29:05 +0900

    Display the custom error page

--- app/src/main/java/io/github/hugehoge/sample/MainActivity.kt
+++ app/src/main/java/io/github/hugehoge/sample/MainActivity.kt
@@ -1,5 +1,6 @@
 package io.github.hugehoge.sample
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.*
@@ -10,6 +11,15 @@ class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy {
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
+    // TODO: Move to companion object
+    private val displayErrorContentCode by lazy {
+        val html = assets.open("error.html")
+                .bufferedReader()
+                .readText()
+
+        """document.documentElement.innerHTML = `$html`"""
     }
 
 
@@ -64,20 +74,42 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun setupWebView() {
+        // This setting is required
         binding.webView.settings.javaScriptEnabled = true
+        // for debug
+        binding.webView.settings.cacheMode = WebSettings.LOAD_NO_CACHE
 
-        binding.webView.webViewClient = object : WebViewClient() {
-            override fun onReceivedError(
-                    view: WebView?,
-                    request: WebResourceRequest?,
-                    error: WebResourceError?
-            ) {
-                super.onReceivedError(view, request, error)
+        binding.webView.webViewClient =
+                if (Build.VERSION.SDK_INT < 23) {
+                    object : WebViewClient() {
+                        override fun onReceivedError(
+                                view: WebView?,
+                                errorCode: Int,
+                                description: String?,
+                                failingUrl: String?
+                        ) {
+                            super.onReceivedError(view, errorCode, description, failingUrl)
 
-                view?.evaluateJavascript(DOM_REPLACE_JS, null)
-                view?.stopLoading()
-            }
-        }
+                            view?.evaluateJavascript(displayErrorContentCode, null)
+                            view?.stopLoading()
+                        }
+                    }
+                } else {
+                    object : WebViewClient() {
+                        override fun onReceivedError(
+                                view: WebView?,
+                                request: WebResourceRequest?,
+                                error: WebResourceError?
+                        ) {
+                            super.onReceivedError(view, request, error)
+
+                            if (request?.isForMainFrame == true) {
+                                view?.evaluateJavascript(displayErrorContentCode, null)
+                                view?.stopLoading()
+                            }
+                        }
+                    }
+                }
 
         binding.webView.webChromeClient = object : WebChromeClient() {
             override fun onProgressChanged(view: WebView?, newProgress: Int) {
@@ -91,8 +123,5 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val INITIAL_PAGE_URL = "https://www.example.com/"
-
-        private const val ERROR_PAGE_HTML = ""
-        private const val DOM_REPLACE_JS = "document.documentElement.innerHTML = '$ERROR_PAGE_HTML'"
     }
 }
